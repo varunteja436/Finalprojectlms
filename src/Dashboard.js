592968353f301ac7e4d40cc3bce519b8618771e7
2092062 <akhilreddychilukuri@gmail.com>
2025-01-14 15:55:47 -0500

    updatee educatorlist.css and dashboard css

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getAuth, signOut } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
+import "./Dashboard.css";
 
 const Dashboard = () => {
   const [name, setName] = useState("");
@@ -32,13 +33,20 @@ const Dashboard = () => {
 
   return (
     <div className="dashboard-container">
-      <h1>Welcome, {name}!</h1>
       <aside>
         <ul>
           <li><Link to="/educatorlist">Educatorlist</Link></li>
           <li><Link to="/">Logout</Link></li>
         </ul>
       </aside>
+      <main className="dashboard-main">
+        <header>
+          <h1>Admin Dashboard</h1>
+          </header>   
+            <section className="dashboard-section">
+        <h1>Welcome, {name}!</h1>
+        </section>
+      </main>
     </div>
   );
 };
--- src/Educatorlist.js
+++ src/Educatorlist.js
@@ -1,21 +1,33 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./Educatorlist.css";
 
 const EducatorList = () => {
   const navigate = useNavigate();
 
   return (
     <div className="educator-list-container">
-        <div className="header-container">
-          <p className="header-text">Admin Educator List</p>
+      <aside>
+        <ul>
+          <li><Link to="/educatorlist">Educatorlist</Link></li>
+          <li><Link to="/">Logout</Link></li>
+        </ul>
+      </aside>
+      <main className="educator-list-main">
+        <header>
+          <h1>Admin Educator List</h1>
+        </header>
+        <section className="educator-list-section">
           <button
             className="create-educator-button"
             onClick={() => navigate("/create-educator")}
           >
             Create Educator
           </button>
-        </div>
-      </div>
+        </section>
+      </main>
+    </div>
   );
 };
 
