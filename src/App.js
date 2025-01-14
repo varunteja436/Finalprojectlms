b6849844b0853e2038114e4bcd856de26093c0fa
varunteja436 <varuntej436@gmail.com>
2025-01-14 01:04:27 -0500

    updated login page, dahsboard,image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import Login from './Login';
-
-function App() {
+import React from "react";
+import "./App.css";
+import Login from "./Login"; 
+import { BrowserRouter, Route, Routes } from "react-router-dom"; 
+import Dashboard from "./Dashboard"; function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </Router>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
-export default App;
+export default App;
