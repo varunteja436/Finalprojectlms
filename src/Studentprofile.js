b1443b95caa0f20fe976a5a796dcd7b1956076f5
2092062 <akhilreddychilukuri@gmail.com>
2025-01-15 17:45:35 -0500

    changed profile to view profile

--- src/Studentprofile.js
+++ src/Studentprofile.js
@@ -67,7 +67,7 @@ const StudentProfile = () => {
     <div className="student-profile-container">
       <aside>
         <ul>
-          <li><Link to="/studentprofile">Profile</Link></li>
+          <li><Link to="/studentprofile"> View Profile</Link></li>
         </ul>  
          <ul>
           <li><Link to="/">Logout</Link></li>
