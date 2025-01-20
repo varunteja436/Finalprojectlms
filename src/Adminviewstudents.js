1a70d28ba7d294345ffb89049d66a364c16f3fd3
Rajesh Reddy <rajeshreddy@Rajeshs-MacBook-Air-2.local>
2025-01-20 18:42:25 -0500

    added suspend function in adminviewstudents

--- src/Adminviewstudents.js
+++ src/Adminviewstudents.js
@@ -1,38 +1,57 @@
 import React, { useEffect, useState } from "react";
-import { getDatabase, ref, get } from "firebase/database";
-import { Link, useNavigate } from "react-router-dom";
+import { getDatabase, ref, get, update } from "firebase/database";
+import { Link } from "react-router-dom";
+import { db } from "./firebase";
+import "./Adminviewstudents.css";
 
 
 const AdminViewStudents = () => {
-  const navigate = useNavigate();
   const [studentList, setStudentList] = useState(null);
 
   useEffect(() => {
-    const fetchStudents = async () => {
-      const database = getDatabase();
-      const usersRef = ref(database, "users");
-
-      try {
-        const data = await get(usersRef);
-
-        if (data.exists()) {
-          const allUsers = data.val();
-          const students = {};
-
-          for (const uid in allUsers) {
-            if (allUsers[uid].role === "STUDENT") {
-              students[uid] = allUsers[uid];
-            }
-          }
-
-          setStudentList(students);
-        }
-      } catch (error) {
-        console.error("Error fetching students:", error);
-      }
-    };
     fetchStudents();
   }, []);
+
+  const fetchStudents = async () => {
+    const database = getDatabase();
+    const usersRef = ref(database, "users"); 
+
+    try {
+      const snapshot = await get(usersRef);
+
+      if (snapshot.exists()) {
+        const allUsers = snapshot.val(); 
+        const students = {};
+
+
+        for (const uid in allUsers) {
+          if (allUsers[uid].role === "STUDENT") {
+            students[uid] = allUsers[uid]; 
+          }
+        }
+
+        console.log("Students:", students);
+        setStudentList(students); 
+      } else {
+        console.log("No users found.");
+        return null;
+      }
+    } catch (error) {
+      console.error("Error fetching students:", error);
+    }
+  };
+
+  const deactivateUserById = async (userId, flag) => {
+    const userRef = ref(db, `users/${userId}`);
+
+    try {
+      await update(userRef, { isActive: flag });
+      await fetchStudents();
+      alert("User deactivated successfully!");
+    } catch (error) {
+      console.error("Error deactivating user:", error);
+    }
+  };
 
   return (
     <div className="admin-view-students-container">
@@ -45,6 +64,16 @@ const AdminViewStudents = () => {
         <ul>
           <li>
             <Link to="/adminstudentslist">Student List</Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to="/educatorlist">Educator List</Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to="/adminviewcontent"> View Content</Link>
           </li>
         </ul>
         <ul>
@@ -62,11 +91,13 @@ const AdminViewStudents = () => {
         <header>
           <h1>Admin Students List</h1>
         </header>
+
         <section className="admin-view-students-section">
           <table border="1">
             <thead>
               <tr>
                 <th>Full Name</th>
+                <th>Action</th>
               </tr>
             </thead>
             <tbody>
@@ -74,6 +105,27 @@ const AdminViewStudents = () => {
                 Object.keys(studentList).map((key) => (
                   <tr key={key}>
                     <td>{studentList[key].name}</td>
+                    <td>
+                      {studentList[key]?.isActive ? (
+                        <button
+                          className="suspend-btn"
+                          onClick={() =>
+                            deactivateUserById(studentList[key]?.uid, false)
+                          }
+                        >
+                          Suspend
+                        </button>
+                      ) : (
+                        <button
+                          className="active-student-btn"
+                          onClick={() =>
+                            deactivateUserById(studentList[key]?.uid, true)
+                          }
+                        >
+                          Activate Student
+                        </button>
+                      )}
+                    </td>
                   </tr>
                 ))}
             </tbody>
