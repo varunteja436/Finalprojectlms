8c7e309c4e5122fcc121a123230f6ca1fd3f44bc
varunteja436 <varuntej436@gmail.com>
2025-01-20 22:43:02 -0500

    adding available course details section

--- src/StudentCourse.js
+++ src/StudentCourse.js
@@ -2,13 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 
+const AvailableCourseCard = ({ course, onViewDetails }) => {
+  return (
+    <div className="student-AvailableCourse-card">
+      <div className="student-AvailableCourse-name">{course.title}</div>
+      <button
+        className="student-AvailableCourse-btn"
+        onClick={onViewDetails}
+      >
+        Enroll Now
+      </button>
+    </div>
+  );
+};
+
 const StudentCourse = () => {
   return (
     <aside>
       <ul>
         <li><Link to="/studentdashboard">Home</Link></li>
-        <li><Link to="/studentcourse">Courses</Link></li>
+        
         <li><Link to="/studentprofile">View Profile</Link></li>
+        <li><Link to="/studentcourses">View Course</Link></li>
         <li><Link to="/">Logout</Link></li>
       </ul>
     </aside>
