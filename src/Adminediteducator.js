a7c1cb4c04e3ed9edb4ce60c6a032ee69ee05653
rajeshreddy0201 <rajeshreddy9177@gmail.com>
2025-01-20 19:15:39 -0500

    added sidebar in adminediteducator

--- src/Adminediteducator.js
+++ src/Adminediteducator.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 const AdminEditEducator = () => {
   return (
     <div className="admin-educator-profile-container">
+      <aside>
+        <ul>
+          <li>
+            <Link to="/admindashboard">Home</Link>
+          </li>
+          <li>
+            <Link to="/adminstudentslist">Student List</Link>
+          </li>
+          <li>
+            <Link to="/educatorlist">Educator List</Link>
+          </li>
+          <li>
+            <Link to="/">Logout</Link>
+          </li>
+        </ul>
+      </aside>
       <main className="admin-educator-profile-main">
         <header>
           <h1>Edit Educator</h1>
         </header>
-        <div>Educator details will go here</div>
+        <div></div>
       </main>
     </div>
   );
